4188c6c37e6a358b5801c0b56b159d8afeb23946
David Bailey <davidbailey.2889@gmail.com>
2019-10-12 23:06:01 +0200

    Use the new elements in the app to test

--- src/App.js
+++ src/App.js
@@ -2,25 +2,104 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ActiveConnection from './GameConnection.js'
+
+import GenericBar from './GenericBar.js'
+
+import {SmallPlayerWidget, PlayerList} from './PlayerWidgets.js'
+
+var gameConn = ActiveConnection;
+
+function PlayerTableRow(player) {
+	let className = [];
+	if(!player.connected)
+		className.push("GreyedOut");
+
+	return <tr key={player.deviceID} className={className.join(" ")}>
+		<td> {player.deviceID} </td>
+		<td> {player.name} </td>
+		<td> {player.team} </td>
+		<td> {player.life} </td>
+		<td> {player.brightness} </td>
+
+		<td> {player.ping} </td>
+		<td> {player.battery} </td>
+	</tr>
+}
+
+function compPlayers(a, b) {
+	const partPlayers = gameConn.game.participatingPlayers;
+
+	if(a.deviceID === "Test")
+		return 1;
+
+	if(a.connected !== b.connected)
+		return a.connected ? -1 : 1;
+	if(partPlayers.includes(a) !== partPlayers.includes(b))
+		return partPlayers.includes(a) ? -1 : 1;
+	if(a.team !== b.team)
+		return (a.team < b.team) ? -1 : 1;
+	else
+		return (a.deviceID > b.deviceID);
+}
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			players: [],
+			targetPlayer: gameConn.get_player("BC.DD.C2.D0.63.F8"),
+		}
+	}
+
+	componentDidMount = () => {
+		gameConn.hook_in(this);
+	}
+
+	onPlayerUpdate = () => {
+		this.setState({players: gameConn.players, targetPlayer: gameConn.get_player("BC.DD.C2.D0.63.F8")});
+	}
+
+	render() {
+		let playerList = [].concat(Object.values(this.state.players));
+		playerList.sort(compPlayers);
+
+		let playerListItems = playerList.map((player) =>
+			PlayerTableRow(player)
+		);
+
+		return (
+			<div className="App">
+
+				<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+				Edit <code>src/App.js</code> and save to reload.
+				<br />
+				Team Value:
+					<PlayerList players={playerList} listName={"Connected Players"}/>
+				</p>
+				<table className="PlayerTable">
+					<thead>
+						<tr>
+							<th> Device ID </th>
+							<th> Name </th>
+							<th> Team </th>
+							<th> Life </th>
+							<th> Brightness </th>
+							<th> Ping </th>
+							<th> VBat </th>
+						</tr>
+					</thead>
+					<tbody>
+						{playerListItems}
+					</tbody>
+				</table>
+			</header>
+			</div>
+		);
+	}
 }
 
 export default App;
